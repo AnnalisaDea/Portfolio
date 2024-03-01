1ca63688c49b5345bbfe501cb3acad562453afd0
annalisa <annalisa.deanesi@gmail.com>
2024-03-01 10:09:51 +0100

    scrollToTopBtn JS

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const scrollToTopBtn = document.getElementById("back-top");
+
+function scrollUp() {
+    if (window.scrollY > 100) { // Mostra il pulsante dopo uno scroll di 100 pixel
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Mostra il pulsante per tornare in cima alla pagina
+    window.addEventListener('scroll', scrollUp);
+})
 
 
 
