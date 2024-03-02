bc923ad226e7bcd84932cb309e76387821eb684c
annalisa <annalisa.deanesi@gmail.com>
2024-03-02 17:54:13 +0100

    Semplificazione JS

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function scrollUp() {
 
 function activeChips(e) {
     //Guardo quale chips era attiva prima e tolgo la classe "active"
-    let beforeChipActive = document.querySelector(".chips.active")
-    beforeChipActive.className = beforeChipActive.className.replace(" active", "");
+    let beforeChipActive = document.querySelector(".chips.active");
+    beforeChipActive.classList.remove("active");
 
 
     //Guardo quale chips è stata cliccata e aggiungo la classe "active"
     let cuttentChipActive = e.target;
-    cuttentChipActive.className += " active";
+    cuttentChipActive.classList.add("active");
 
 
     //Guardo qual è il valore di 'data-filter' della chips cliccata
@@ -30,7 +30,7 @@ function activeChips(e) {
 
     //filtro in base al valore della chips
     galleryImages.forEach(image => {
-        if (filterValue === 'all' || image.dataset.category === filterValue) {
+        if (filterValue == 'all' || image.dataset.category == filterValue) {
             image.style.display = 'block';
         } else {
             image.style.display = 'none';
